ed0f062ee002bdbe6a61a4d3f6cfb28f332d81ca
Soomin Kim <soomink@kaist.ac.kr>
2025-11-19 14:18:45 +0900

    Update main script

--- scripts/run_DAFL.sh
+++ scripts/run_DAFL.sh
@@ -4,13 +4,28 @@ TARGET=$1
 TIMEOUT=$2
 ITERATION=$3
 
+# Download
 cd /projects/$1
 if [ ! -d SRC ]; then
     ./download.sh
 fi
-./run_smake.sh
-python3 /scripts/run_sparrow.py $TARGET thin
-python3 /scripts/run_sparrow.py $TARGET naive
-./build_DAFL.sh
+# Analyze
+if [ ! -d smake ]; then
+    ./run_smake.sh
+fi
+if [ ! -d DAFL-input ]; then
+    python3 /scripts/run_sparrow.py $TARGET thin
+fi
+if [ ! -d DAFL-input-naive ]; then
+    python3 /scripts/run_sparrow.py $TARGET naive
+fi
+# Build
+if [ ! -d bin ]; then
+    ./build_DAFL.sh
+fi
+if [ ! -d asan ]; then
+    ./build_ASAN.sh
+fi
+# Fuzz
 /scripts/prepare.sh $TARGET
 python3 /scripts/run.py $TARGET $TIMEOUT $ITERATION
